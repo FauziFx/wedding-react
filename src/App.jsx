c10c7953de9c90ffad595ca9ca570903f7badb23
Ahmad Fauzi <fauzitkj3@gmail.com>
2025-01-16 13:14:14 +0700

    feat: add copy content account number

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ function App() {
   const [guest, setGuest] = useState(params.get("to"));
   const [open, setOpen] = useState(false);
   const [showConvetti, setShowConvetti] = useState(false);
+  const [salin, setSalin] = useState(false);
 
   useEffect(() => {
     document.title = "The Wedding";
@@ -55,6 +56,19 @@ function App() {
       setTheme("dark");
     } else {
       setTheme("light");
+    }
+  };
+
+  const copyContent = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setSalin(true);
+      const timer = setTimeout(() => {
+        setSalin(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
     }
   };
 
@@ -515,17 +529,26 @@ function App() {
                 </div>
                 <div>
                   <div className="text-xs">
-                    <button className="btn btn-outline btn-xs">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="h3 w-3"
-                      >
-                        <path d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 0 1 3.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0 1 21 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 0 1 7.5 16.125V3.375Z" />
-                        <path d="M15 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 17.25 7.5h-1.875A.375.375 0 0 1 15 7.125V5.25ZM4.875 6H6v10.125A3.375 3.375 0 0 0 9.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V7.875C3 6.839 3.84 6 4.875 6Z" />
-                      </svg>
-                      Salin
+                    <button
+                      className="btn btn-outline btn-xs"
+                      onClick={() => copyContent("123456789")}
+                    >
+                      {salin ? (
+                        "Tersalin"
+                      ) : (
+                        <>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="h3 w-3"
+                          >
+                            <path d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 0 1 3.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0 1 21 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 0 1 7.5 16.125V3.375Z" />
+                            <path d="M15 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 17.25 7.5h-1.875A.375.375 0 0 1 15 7.125V5.25ZM4.875 6H6v10.125A3.375 3.375 0 0 0 9.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V7.875C3 6.839 3.84 6 4.875 6Z" />
+                          </svg>
+                          Salin
+                        </>
+                      )}
                     </button>
                   </div>
                 </div>
