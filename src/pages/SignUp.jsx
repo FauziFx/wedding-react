c62cf789a3e64dc26bef0201f867d76611facd52
Ahmad Fauzi <fauzitkj3@gmail.com>
2025-05-16 16:00:53 +0700

    refactor: attributes svg

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -153,7 +153,7 @@ function SignUp() {
                     cy="12"
                     r="10"
                     stroke="currentColor"
-                    stroke-width="4"
+                    strokeWidth="4"
                   ></circle>
                   <path
                     className="opacity-75"
